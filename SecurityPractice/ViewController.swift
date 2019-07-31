99cdc0f7bc3df5d311737e3fc29c6725080672a0
Nicholas Cooke <ncooke3@gatech.edu>
2019-07-31 16:17:29 -0700

    Initial Commit

---
+++ SecurityPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecurityPractice
+//
+//  Created by Nicholas Cooke on 7/31/19.
+//  Copyright © 2019 Nicholas Cooke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
