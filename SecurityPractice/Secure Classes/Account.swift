361886e8777f3947f4e54feabb39da66eb193d39
Nicholas Cooke <ncooke3@gatech.edu>
2019-07-31 22:53:09 -0700

    feat(Account): made Account conform to Locksmith protocols

--- SecurityPractice/Secure Classes/Account.swift
+++ SecurityPractice/Secure Classes/Account.swift
@@ -7,15 +7,30 @@
 //
 
 import Foundation
+import Locksmith
 
-class Account {
-    var service : String
+class Account:  ReadableSecureStorable,
+                CreateableSecureStorable,
+                DeleteableSecureStorable,
+                GenericPasswordSecureStorable {
+    
+    //var service : String
     var username: String
     var password: String
+    
+    //Required by GenericPasswordSecureStorable
+    let service: String
+    var account: String { return username }
+    
+    //Required by CreateableSecureStorable
+    var data: [String: Any] {
+        return ["password": password]
+    }
     
     init(service: String, username: String, password: String) {
         self.service = service
         self.username = username
         self.password = password
     }
+    
 }
