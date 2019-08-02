189f5ffb556da8bd77933e9513dfadd85eaec85d
Nicholas Cooke <ncooke3@gatech.edu>
2019-08-01 20:19:05 -0700

    feat(Account): added keychain, username, and password instance methods
    Keychain
    • safelyStoreInKeychain()
    • safelyDeleteFromKeychain()
    
    AccountDefaults
    • addToAccountDefaults()
    • removeFromAccountDefaults(at index:)
    
    Password
    • getPasswordFromStore()
    • setPasswordInStore(newPassword:)

--- SecurityPractice/Secure Classes/Account.swift
+++ SecurityPractice/Secure Classes/Account.swift
@@ -33,6 +33,56 @@ class Account:  ReadableSecureStorable,
         self.username = username
         self.password = password
     }
+    
+    func safelyStoreInKeychain() {
+        do {
+            try self.createInSecureStore()
+        } catch {
+            print("❌ Couldn't store new account in keychain: \(error)")
+        }
+        
+        // After storing password in keychain, let's clear password!
+        self.password = ""
+    }
+    
+    func safelyDeleteFromKeychain() {
+        do {
+            try self.deleteFromSecureStore()
+        } catch {
+            print("❌ Couldn't delete account in keychain: \(error)")
+        }
+    }
+    
+    func addToAccountsDefaults() {
+        AccountDefaults.accounts.append(self)
+    }
+    
+    // TODO: This probabaly doesnt need to be an instance method of Account
+    func removeFromAccountDefaults(at index: Int) {
+        AccountDefaults.accounts.remove(at: index)
+    }
+    
+    // Lanyard/DetailVC: Load password to populate passwordTextField
+    func getPasswordFromStore() -> String? {
+        if let keychainData = self.readFromSecureStore() {
+            if let dictionary = keychainData.data {
+                if let password = dictionary["password"] as? String {
+                    print("The password for \(self.service) is: \(password).")
+                    return password
+                }
+            }
+        }
+        return nil
+    }
+    
+    func setPasswordInStore(newPassword: String) {
+        self.password = newPassword
+        do {
+            try self.updateInSecureStore()
+        } catch {
+            print("❌ Error: \(error)")
+        }
+    }
 
     private enum CodingKeys: String, CodingKey {
         case service
