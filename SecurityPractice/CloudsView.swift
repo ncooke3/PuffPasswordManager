13538520912ccf55986b3c53ac97c6a07816c910
Nicholas Cooke <ncooke3@gatech.edu>
2019-08-13 11:23:48 -0400

    feat(SecurityPractice): added CloudsView
    • will serve as animated background

---
+++ SecurityPractice/CloudsView.swift
@@ -0,0 +1,28 @@
+//
+//  CloudsView.swift
+//  SecurityPractice
+//
+//  Created by Nicholas Cooke on 8/13/19.
+//  Copyright © 2019 Nicholas Cooke. All rights reserved.
+//
+
+import UIKit
+
+class CloudsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.addSubview(FloatingClouds())
+    }
+    
+
+
+}
+
+class FloatingClouds: UIView {
+    
+    
+    
+}
