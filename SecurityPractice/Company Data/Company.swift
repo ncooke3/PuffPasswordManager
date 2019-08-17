1877f121cbcc81daf311c2d675a29a33b41b1ddd
Nicholas Cooke <ncooke3@gatech.edu>
2019-08-17 08:03:02 -0400

    feat(Company): added company and companyDefaults struct

---
+++ SecurityPractice/Company Data/Company.swift
@@ -0,0 +1,15 @@
+//
+//  Company.swift
+//  SecurityPractice
+//
+//  Created by Nicholas Cooke on 8/17/19.
+//  Copyright © 2019 Nicholas Cooke. All rights reserved.
+//
+
+import Foundation
+
+struct Company: Codable {
+    var name: String
+    var url: URL
+    var color: String?
+}
---
+++ SecurityPractice/Company Data/CompanyDefaults.swift
@@ -0,0 +1,24 @@
+//
+//  CompanyDefaults.swift
+//  SecurityPractice
+//
+//  Created by Nicholas Cooke on 8/17/19.
+//  Copyright © 2019 Nicholas Cooke. All rights reserved.
+//
+
+import Foundation
+
+struct CompanyDefaults {
+    static private let companiesKey = "companiesKey"
+    
+    static var companies: [String: Company] = {
+        guard let data = UserDefaults.standard.data(forKey: companiesKey) else { return [:] }
+        return try! JSONDecoder().decode([String: Company].self, from: data)
+        
+    }() {
+        didSet {
+            guard let data = try? JSONEncoder().encode(companies) else { return }
+            UserDefaults.standard.set(data, forKey: companiesKey)
+        }
+    }
+}
